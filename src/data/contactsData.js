e305f3ed109c78b694c862b4c67f0823e49c4eb5
Shawn Zhou <feshawnzhou@gmail.com>
2024-06-07 00:38:30 -0700

    update address

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'feshawnzhou@gmail.com',
-    address: 'Irvine, California',
+    address: 'Los angeles, California',
 
     sheetAPI: process.env.REACT_APP_SHEET_API
 }
