1814c2c8181d177d0abadbda6aad76898d694bb1
Shawn Zhou <feshawnzhou@gmail.com>
2024-06-07 00:31:36 -0700

    fix BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { Main, ProjectPage } from './pages'
-import { BackToTop } from './components'
-import ScrollToTop from './utils/ScrollToTop'
-import './App.css'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Main, ProjectPage } from './pages';
+import { BackToTop } from './components';
+import ScrollToTop from './utils/ScrollToTop';
+import './App.css';
 
 function App() {
-
   return (
     <div className="app">
       <BrowserRouter basename="/shawn-portfolio">
-        <Router>
-          <ScrollToTop/>
-          <Switch>
-            <Route path="/" exact component={Main} />
-            <Route path="/projects" exact component={ProjectPage} />
-            <Redirect to="/" />
-          </Switch>
-        </Router>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={Main} />
+          <Route path="/projects" exact component={ProjectPage} />
+          <Redirect to="/" />
+        </Switch>
         <BackToTop />
       </BrowserRouter>
     </div>
